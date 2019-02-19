99ab39f0a20c5b67a4f28f04af613b4f0536abf8
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2019-02-19 15:45:31 +0100

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,12 +4,22 @@ const request = require("supertest");
 const { app } = require("./../server");
 const { Todo } = require("./../models/todo");
 
+const todos = [
+  {
+    text: "First test todo"
+  },
+  {
+    text: "second test todo"
+  }
+];
 //Usuwa wszystko z bazy danych na potrzebe testu
 //raczej niebezpieczna funkcja xDDD
 beforeEach(done => {
-  Todo.remove({}).then(() => {
-    done();
-  });
+  Todo.remove({})
+    .then(() => {
+      return Todo.insertMany(todos);
+    })
+    .then(() => done());
 });
 
 describe("POST /todos", () => {
@@ -29,7 +39,7 @@ describe("POST /todos", () => {
         }
 
         //now if everything above was good, try to fetch data from database
-        Todo.find()
+        Todo.find({ text }) //FOR GET TEST - zeby byl rowny tylko temu textowi ktory znajdzie w mongo a nie wszystko co ma tam
           .then(todos => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
@@ -48,7 +58,7 @@ describe("POST /todos", () => {
     request(app)
       .post("/todos")
       .send({ text })
-      .expect(400)
+      .expect(400) // 400 bo chce uzyskac bad request
       .end((err, res) => {
         if (err) {
           return done(err);
@@ -57,7 +67,7 @@ describe("POST /todos", () => {
         //now if everything above was good, try to fetch data from database
         Todo.find()
           .then(todos => {
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2); // Should be 0 //GET TEST 2 bo do get testuje 2 dokumenty
             done();
           })
           .catch(e => {
@@ -65,5 +75,17 @@ describe("POST /todos", () => {
             done(e);
           });
       });
+  });
+});
+
+describe("GET /todos", () => {
+  it("should get all todos", done => {
+    request(app)
+      .get("/todos")
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
