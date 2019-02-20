cb3c79b3a327a868bc31c140026144e29fa67858
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2019-02-20 16:30:14 +0100

    try to deploy 2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,9 @@ var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 var uri =
-  "mongodb://karol2tomek:haslotestowe@cluster0-shard-00-00-royto.mongodb.net:27017,cluster0-shard-00-01-royto.mongodb.net:27017,cluster0-shard-00-02-royto.mongodb.net:27017/ToooApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+  "mongodb://" +
+  process.todoapp_db +
+  "@cluster0-shard-00-00-royto.mongodb.net:27017,cluster0-shard-00-01-royto.mongodb.net:27017,cluster0-shard-00-02-royto.mongodb.net:27017/ToooApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
 mongoose.connect(uri);
 
 module.exports = {
