58260d8534b35bfe097fbfe75ebe129f5d08c53e
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2019-02-20 15:32:27 +0100

    Try to deploy 1

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,10 @@ var mongoose = require("mongoose");
 //mongoode działa defoultowo na callbackach, ale tu będzie pokazane na promisach:
 //Ustawiam dla mongoose'a jakiej biblioteki do promisów będe uzywał:
 mongoose.Promise = global.Promise;
-//mongoose.connect(process.env.MONGDB_URI || `mongodb://localhost:27017/TodoApp`);
+mongoose.connect(
+  process.env.MONGDB_URI ||
+    "mongodb://karol2tomek:banany26@cluster0-shard-00-00-royto.mongodb.net:27017,cluster0-shard-00-01-royto.mongodb.net:27017,cluster0-shard-00-02-royto.mongodb.net:27017/test?ssl=true"
+);
 
 module.exports = {
   mongoose
