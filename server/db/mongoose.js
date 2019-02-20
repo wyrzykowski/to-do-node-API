30890bafe6d27fd9e50c28039c31fda7825e1de7
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2019-02-20 16:18:14 +0100

    try to deploy 2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,9 +3,10 @@ var mongoose = require("mongoose");
 //mongoode działa defoultowo na callbackach, ale tu będzie pokazane na promisach:
 //Ustawiam dla mongoose'a jakiej biblioteki do promisów będe uzywał:
 mongoose.Promise = global.Promise;
-mongoose.connect(
-  "mongodb://karol2tomek:haslotestowe@cluster0-shard-00-00-royto.mongodb.net:27017,cluster0-shard-00-01-royto.mongodb.net:27017,cluster0-shard-00-02-royto.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
-);
+
+var uri =
+  "mongodb://karol2tomek:haslotestowe@cluster0-shard-00-00-royto.mongodb.net:27017,cluster0-shard-00-01-royto.mongodb.net:27017,cluster0-shard-00-02-royto.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+mongoose.connect(uri);
 
 module.exports = {
   mongoose
