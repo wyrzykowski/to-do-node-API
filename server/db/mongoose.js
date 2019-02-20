53cfc9f03fe3824126b3a07811dd09f54d5e83a3
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2019-02-20 16:32:50 +0100

    try to deploy 2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,7 +6,7 @@ mongoose.Promise = global.Promise;
 
 var uri =
   "mongodb://" +
-  process.todoapp_db +
+  process.env.todoapp_db +
   "@cluster0-shard-00-00-royto.mongodb.net:27017,cluster0-shard-00-01-royto.mongodb.net:27017,cluster0-shard-00-02-royto.mongodb.net:27017/ToooApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
 mongoose.connect(uri);
 
