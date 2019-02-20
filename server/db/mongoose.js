69b486f60fbffbe647259653bf33a4d6d0f0fbf2
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2019-02-20 16:38:48 +0100

    ready to deploy

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,10 +4,14 @@ var mongoose = require("mongoose");
 //Ustawiam dla mongoose'a jakiej biblioteki do promisów będe uzywał:
 mongoose.Promise = global.Promise;
 
-var uri =
-  "mongodb://" +
-  process.env.todoapp_db +
-  "@cluster0-shard-00-00-royto.mongodb.net:27017,cluster0-shard-00-01-royto.mongodb.net:27017,cluster0-shard-00-02-royto.mongodb.net:27017/ToooApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+if (process.env.todoapp_db) {
+  var uri =
+    "mongodb://" +
+    process.env.todoapp_db +
+    "@cluster0-shard-00-00-royto.mongodb.net:27017,cluster0-shard-00-01-royto.mongodb.net:27017,cluster0-shard-00-02-royto.mongodb.net:27017/ToooApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+} else {
+  var uri = "mongodb://localhost:27017/TodoApp";
+}
 mongoose.connect(uri);
 
 module.exports = {
