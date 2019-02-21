bac2a83f4541c6ca3edccd9b5f48094ca1be67e8
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2019-02-21 15:51:11 +0100

    add in server.js that in case of env.NODE_ENV address of mogodb will change: developmnet test and deploy"

--- server/server.js
+++ server/server.js
@@ -1,3 +1,19 @@
+var env = process.env.NODE_ENV; // ta zmienna środowiskowa dostępna tylko na heroku
+
+console.log("#@@@@@@@@#@#@##@#@#@#@#@#@#", env);
+if (env === "development") {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp";
+} else if (env === "test") {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApptest";
+} else {
+  process.env.MONGODB_URI =
+    "mongodb://" +
+    process.env.todoapp_db +
+    "@cluster0-shard-00-00-royto.mongodb.net:27017,cluster0-shard-00-01-royto.mongodb.net:27017,cluster0-shard-00-02-royto.mongodb.net:27017/ToooApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+}
+
 const _ = require("lodash");
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -8,7 +24,7 @@ var { Todo } = require("./models/todo");
 var { Users } = require("./models/user");
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json()); // set podsy parser to be use by express app
 //dzięki temu moge wysyłać coś w postaci JSONA do tej aplikacji, bo będzie mi
