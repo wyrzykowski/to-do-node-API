0f552837edbcd21e2863e4f35a6b5ac29a5a0bd2
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2019-02-27 22:21:37 +0100

    update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,15 +1,24 @@
 var env = process.env.NODE_ENV; // ta zmienna środowiskowa dostępna tylko na heroku
 
-console.log("#@@@@@@@@#@#@##@#@#@#@#@#@#", env);
-if (env === "development") {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp";
-} else if (env === "test") {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApptest";
-} else {
-  process.env.MONGODB_URI =
-    "mongodb://" +
-    process.env.todoapp_db +
-    "@cluster0-shard-00-00-royto.mongodb.net:27017,cluster0-shard-00-01-royto.mongodb.net:27017,cluster0-shard-00-02-royto.mongodb.net:27017/ToooApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+if (env === "development" || env === "test") {
+  var config = require("./config.json");
+  var envConfig = config[env]; //grap proper env configuration from config.json
+
+  Object.keys(envConfig).forEach(key => {
+    //Object.keys return keys from argument
+    process.env[key] = envConfig[key];
+  });
 }
+
+// if (env === "development") {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp";
+// } else if (env === "test") {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApptest";
+// } else {
+//   process.env.MONGODB_URI =
+//     "mongodb://" +
+//     process.env.todoapp_db +
+//     "@cluster0-shard-00-00-royto.mongodb.net:27017,cluster0-shard-00-01-royto.mongodb.net:27017,cluster0-shard-00-02-royto.mongodb.net:27017/ToooApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+// }
--- server/models/user.js
+++ server/models/user.js
@@ -48,7 +48,7 @@ UserSchema.methods.generateAuthToken = function() {
   var user = this;
   var access = "auth";
   var token = jwt
-    .sign({ _id: user._id.toHexString(), access }, "abc123")
+    .sign({ _id: user._id.toHexString(), access }, process.env.JWT_SECRET)
     .toString();
 
   user.tokens = user.tokens.concat([{ access, token }]);
@@ -63,7 +63,7 @@ UserSchema.statics.findByToken = function(token) {
   var decoded;
 
   try {
-    decoded = jwt.verify(token, "abc123");
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     return Promise.reject(); //skrócona wersja Promise
   }
